8d394c7a6f5b198b6187c15aff8dc5ccfc0ed036
ChenShenAi <chenshenai@huawei.com>
2026-08-29 18:13:17 +0800

    test(dspark): Isolate layer-40 target tap

--- models/deepseek_v4_flash_dspark/decode_fwd.py
+++ models/deepseek_v4_flash_dspark/decode_fwd.py
@@ -856,6 +856,10 @@ def decode_fwd(
                         ]
 
     with pl.scope():
+        target_hidden_l40 = pl.slice(dspark_target_hidden, [local_t, D], [0, 0])
+        hc_head(x_pong, hc_head_fn, hc_head_scale, hc_head_base, target_hidden_l40)
+
+    with pl.scope():
         csa_ordinal_last = pl.const(20, pl.INT32)
         model_layer_last = pl.const(42, pl.INT32)
         weight_layer_last = model_layer_last % FWD_WEIGHT_BANK_SIZE
